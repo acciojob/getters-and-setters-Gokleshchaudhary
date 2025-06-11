2c06334351fbdf382bbf1cc07095000d0bd13abd
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-06-11 11:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name; // Private variable
+        this._age = age;   // Private variable
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    set age(age) {
+        this._age = age;
+    }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+    get age() {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+
+// Testing the classes
+const person = new Person("John", 25);
+console.log(person.name);  // Output: John
+person.age = 30;           // Using the setter to change the age
+console.log(person.age);    // Output: 30
+
+const student = new Student("Alice", 22);
+student.study();            // Output: Alice is studying
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach();  
