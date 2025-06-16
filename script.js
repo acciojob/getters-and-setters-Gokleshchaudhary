48fd96c0364bd35c884219f5a67fdd4bc5cf8af5
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-06-16 16:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
+class Person {
+	constructor(name, age){
+		this._name=name;
+		this._age=age;
+		
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age=age;
+	}
 
-export class Person {
-    constructor(name, age) {
-        this._name = name; // Private variable
-        this._age = age;   // Private variable
-    }
+	}
+	
+	
 
-    get name() {
-        return this._name;
-    }
 
-    set age(age) {
-        this._age = age;
-    }
-
-    get age() {
-        return this._age;
-    }
+class Student extends Person {
+	study(){
+		console.log(${this._name} is studying);
+	}
+	
 }
 
-export class Student extends Person {
-    study() {
-        console.log(`${this.name} is studying`);
-    }
+class Teacher extends Person {
+	teach(){
+		console.log(${this._name} is teaching)
+	}
 }
-
-export class Teacher extends Person {
-    teach() {
-        console.log(`${this.name} is teaching`);
-    }
-}
-
-// Example Usage
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
-person.age = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach();
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
